fc6455fec1dc52c07a7dbfdcfde36dd351e16d42
Pranav Shirke <pranavshirke@Pranavs-MacBook-Pro.local>
2020-05-24 15:59:15 -0400

    Updates

---
+++ precache-manifest.5c5dc4b10851937e5d5bab7e61b92345.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2673f7ac9efc18efae796239a6293b84",
+    "url": "/myproject/index.html"
+  },
+  {
+    "revision": "ed1753109e957c7a9cfd",
+    "url": "/myproject/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "dee1d03ca26ae2001b3e",
+    "url": "/myproject/static/css/main.5b39e0f2.chunk.css"
+  },
+  {
+    "revision": "ed1753109e957c7a9cfd",
+    "url": "/myproject/static/js/2.a1cd7cfa.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/myproject/static/js/2.a1cd7cfa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dee1d03ca26ae2001b3e",
+    "url": "/myproject/static/js/main.7a36f371.chunk.js"
+  },
+  {
+    "revision": "c680c2e7624d1278efe7",
+    "url": "/myproject/static/js/runtime-main.8ff053f9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myproject/static/media/logo.5d5d9eef.svg"
+  }
+]);
